11d0558f3d75787cc2f9e02774a7f26e69d5a979
nwagenmakers <niels.wagenmakers@dualinventive.com>
2022-11-07 16:55:21 +0100

    adding sub function

--- calc/calc.go
+++ calc/calc.go
@@ -9,4 +9,11 @@ func Add(a, b int) (int, error) {
 		return 0, errors.New("Zero parameters are not allowed!")
 	}
 
+	func Sub(a, b int) (int, error) {
+		if a > 0 && b > 0 {
+			return a - b, nil
+		} else {
+			return 0, errors.New("Zero parameters are not allowed!")
+		}
+
 }
