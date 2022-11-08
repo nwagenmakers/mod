a83cc7b9a16293719e235740245febac5e2cea89
nwagenmakers <niels.wagenmakers@dualinventive.com>
2022-11-08 09:54:23 +0100

    bug fix

--- calc/calc.go
+++ calc/calc.go
@@ -6,14 +6,15 @@ func Add(a, b int) (int, error) {
 	if a > 0 && b > 0 {
 		return a + b, nil
 	} else {
-		return 0, errors.New("Zero parameters are not allowed!")
+		return 0, errors.New("zero parameters are not allowed!")
 	}
+}
 
-	func Sub(a, b int) (int, error) {
-		if a > 0 && b > 0 {
-			return a - b, nil
-		} else {
-			return 0, errors.New("Zero parameters are not allowed!")
-		}
+func Sub(a, b int) (int, error) {
+	if a > 0 && b > 0 {
+		return a - b, nil
+	} else {
+		return 0, errors.New("zero parameters are not allowed!")
+	}
 
 }
